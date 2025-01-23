f4134734ead9e0104c4300bdc619a607108f3f08
klelia <clelia.fradella@gmail.com>
2025-01-23 12:39:13 +0100

    pizza fix

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -15,12 +15,14 @@ function index(req, res) {
     res.json(response);
   });
 }
+function callError() {
+  throw new CustomError("L'elemento non esiste", 404);
+}
 function show(req, res) {
   const id = parseInt(req.params.id);
 
   //prima query per prendere i dati della pizza
   const sql = "SELECT * FROM `pizzas` WHERE `id` = ?";
-
   connection.query(sql, [id], (err, results) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
     const item = results[0];
@@ -46,28 +48,36 @@ WHERE ingredient_pizza.pizza_id = ?`;
 }
 
 function store(req, res) {
-  let newId = 0;
-  for (let i = 0; i < pizzas.length; i++) {
-    if (pizzas[i].id > newId) {
-      newId = pizzas[i].id;
-    }
-  }
-  newId += 1;
-  //console.log(req.body);
-  // new data is in req.body
-  const newItem = {
-    id: newId,
-    ...req.body,
-  };
+  // let newId = 0;
+  // for (let i = 0; i < pizzas.length; i++) {
+  //   if (pizzas[i].id > newId) {
+  //     newId = pizzas[i].id;
+  //   }
+  // }
+  // newId += 1;
+  // //console.log(req.body);
+  // // new data is in req.body
+  // const newItem = {
+  //   id: newId,
+  //   ...req.body,
+  // };
 
-  pizzas.push(newItem);
+  // pizzas.push(newItem);
+  //INSERT INTO tbl_name (a,b,c) VALUES(1,2,3),(4,5,6),(7,8,9); per relazione
   res.status(201).json(newItem);
 }
 
 function update(req, res) {
   const id = parseInt(req.params.id);
   //prima query per prendere i dati della pizza
-
+  // array id ingredients passed in body
+  // previous array id ingredients
+  // toAdd = [] - toRemove = []
+  const toAdd = newIngredients.filter((i) => !previousIngredients.includes(i));
+  const toRemove = previousIngredients.filter((i) => !ingredients.includes(i));
+  // delete from table where id in (3,4,6,8,9) // per relazione
+  // delete from table where id not in (3,4,6,8,9)
+  // INSERT INTO tbl_name (a,b,c) VALUES(1,2,3),(4,5,6),(7,8,9); per relazione
   //console.log(req.body);
   for (key in item) {
     if (key !== "id") {
